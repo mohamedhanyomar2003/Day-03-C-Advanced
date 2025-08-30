2c02f79a97537850746b37b03f2c27a7c2636fb1
compuremo <compuremo@DESKTOP-BIMGJ29>
2025-08-30 16:43:38 +0300

    Q2.B

--- Assignment/LibraryEngine .cs
+++ Assignment/LibraryEngine .cs
@@ -8,7 +8,15 @@ namespace Assignment
 {
     public class LibraryEngine
     {
-        public static void ProcessBooks(List<Book> bList,CustomFunc fPtr)
+        //public static void ProcessBooks(List<Book> bList,CustomFunc fPtr)
+        //{
+        //    foreach (Book b in bList)
+        //    {
+        //        Console.WriteLine(fPtr(b));
+        //    }
+        //}
+
+        public static void ProcessBooks(List<Book> bList, Func<Book,string> fPtr)
         {
             foreach (Book b in bList)
             {
--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -5,7 +5,7 @@
     {
         static void Main(string[] args)
         {
-            #region MyRegion
+            #region Q1.A
             ////User Defined Delegate Datatype
             //List<Book> books = new List<Book>();
             //Book book1 = new Book("123", "shams", ["Ahmed", "Mohamed"], new DateTime(22, 12, 3), 40000m);
@@ -14,6 +14,20 @@
             //books.Add(book2);
             //LibraryEngine.ProcessBooks(books,BookFunctions.GetPrice);
             //LibraryEngine.ProcessBooks(books,BookFunctions.GetAuthors);
+            #endregion
+
+            #region Q2.B
+            //// BCL Delegates
+            //List<Book> books = new List<Book>();
+            //Book book1 = new Book("123", "shams", ["Ahmed", "Mohamed"], new DateTime(22, 12, 3), 40000m);
+            //Book book2 = new Book("123", "Moon", ["Mohamed", "Ali"], new DateTime(22, 12, 3), 77m);
+            //Book book3 = new Book("123", "Earth", ["Mohamed", "Hany"], new DateTime(22, 12, 3), 877m);
+            //books.Add(book1);
+            //books.Add(book2);
+            //books.Add(book3);
+            //LibraryEngine.ProcessBooks(books, BookFunctions.GetPrice);
+            //LibraryEngine.ProcessBooks(books, BookFunctions.GetAuthors);
+            //LibraryEngine.ProcessBooks(books, BookFunctions.GetTitle);
             #endregion
 
         }
